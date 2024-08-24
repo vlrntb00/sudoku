17b686c16de0c74ae48e107ee95001f5062646a6
Aditya Mishra <adityam.rtm@gmail.com>
2024-08-24 20:00:56 +0530

    simplify html

--- script.js
+++ script.js
@@ -2,34 +2,39 @@ function handleEntryClick(event) {
   for (let e of Array.from(document.getElementsByClassName('highlighted-entry'))) {
     e.classList.remove('highlighted-entry');
   }
-  this.classList.add('highlighted-entry');
+  this.parentElement.classList.add('highlighted-entry');
   event.preventDefault();
   return false;
 }
 
-for (let i = 0; i < 9; i++) {
-  window.board.appendChild(document.createElement('tr'));
-  for (let j = 0; j < 9; j++) {
-    window.board.children[i].appendChild(document.createElement('td'));
-    window.board.children[i].children[j].appendChild(document.createElement('table'));
-    let entries = document.createElement('tbody');
-    entries.id = `board${i}${j}`;
-    entries.classList.add('entries');
-    window.board.children[i].children[j].children[0].appendChild(entries);
-    let committedEntry = document.createElement('div');
-    committedEntry.id = `centry${i}${j}`;
-    entries.appendChild(committedEntry);
-    committedEntry.innerText = '0';
-    entries.addEventListener('click', handleEntryClick.bind(entries));
-    for (let k = 1; k < 4; k++) {
-      entries.appendChild(document.createElement('tr'));
-      for (let l = 0; l < 3; l++) {
-        let speculativeEntry = document.createElement('td');
-        speculativeEntry.id = `sentry${i}${j}${k * 3 + l + 1}`;
-        entries.children[k].appendChild(speculativeEntry);
-        entries.children[k].children[l].innerText = k * 3 + l + 1;
-      }
-    }
+for (let i = 0; i < 81; i++) {
+  let entry = document.createElement('div');
+  entry.id = `entry${i}`;
+  entry.classList.add('entry');
+  if (i % 9 === 0) entry.classList.add('newline');
+  if (i % 9 === 2 || i % 9 === 5) entry.classList.add('rightborder');
+  if (Math.floor(i / 9) === 2 || Math.floor(i / 9) === 5) entry.classList.add('bottomborder');
+  window.entries.appendChild(entry);
+  entry.addEventListener('click', handleEntryClick.bind(entry));
+
+  let centry = document.createElement('div');
+  centry.id = `centry${i}`;
+  centry.classList.add('centry');
+  centry.innerText = '0';
+  entry.appendChild(centry);
+
+  let sentries = document.createElement('div');
+  sentries.id = `sentries${i}`;
+  sentries.classList.add('sentries');
+  entry.appendChild(sentries);
+
+  for (let k = 0; k < 9; k++) {
+    let sentry = document.createElement('div');
+    sentry.id = `sentry${k}`;
+    sentry.classList.add('sentry');
+    if (k % 3 === 0) sentry.classList.add('newline');
+    sentry.innerText = k + 1;
+    sentries.appendChild(sentry);
   }
 }
 
