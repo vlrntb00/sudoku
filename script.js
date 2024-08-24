a07a336823b43d8e3e6970c0abfa34ace08c3a44
Aditya Mishra <adityam.rtm@gmail.com>
2024-08-24 18:44:44 +0530

    finish nested table elements

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ for (let i = 0; i < 9; i++) {
     entries.id = `board${i}${j}`;
     entries.classList.add('entries');
     window.board.children[i].children[j].children[0].appendChild(entries);
-    for (let k = 0; k < 3; k++) {
+    let committedEntry = document.createElement('div');
+    committedEntry.id = `centry${i}${j}`;
+    entries.appendChild(committedEntry);
+    committedEntry.innerText = '0';
+    for (let k = 1; k < 4; k++) {
       entries.appendChild(document.createElement('tr'));
       for (let l = 0; l < 3; l++) {
-        entries.children[k].appendChild(document.createElement('td'));
-        entries.children[k].children[l].innerText = k * 3 + l;
+        let speculativeEntry = document.createElement('td');
+        speculativeEntry.id = `sentry${i}${j}${k * 3 + l + 1}`;
+        entries.children[k].appendChild(speculativeEntry);
+        entries.children[k].children[l].innerText = k * 3 + l + 1;
       }
     }
   }
